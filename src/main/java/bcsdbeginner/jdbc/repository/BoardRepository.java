13631ac219614b6c470f7213bf3c7da98d49e1b5
JangTaeHo <lyu84265@gmail.com>
2023-11-24 10:26:02 +0900

    add board add category

---
+++ src/main/java/bcsdbeginner/jdbc/repository/BoardRepository.java
@@ -0,0 +1,136 @@
+package bcsdbeginner.jdbc.repository;
+
+import bcsdbeginner.jdbc.DBConnection.DBConnectionManager;
+import bcsdbeginner.jdbc.domain.Board;
+import lombok.extern.slf4j.Slf4j;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+public class BoardRepository {
+    public Board createBoard(Board newBoard) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        String sql = "insert into board(user_id, category_id, title, content) values(?, ?, ?, ?)";
+
+        try {
+            connection = DBConnectionManager.getConnection();
+            statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, newBoard.getUser_id());
+            statement.setInt(2, newBoard.getCategory_id());
+            statement.setString(3, newBoard.getTitle());
+            statement.setString(4, newBoard.getContent());
+
+            statement.executeUpdate();
+            return newBoard;
+
+        }catch (SQLException e){
+            log.error("createBoard error={}", e);
+            throw e;
+        }finally {
+            closeResource(connection, statement, null);
+        }
+    }
+
+    public Board findById(Integer boardId) throws SQLException{
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String sql = "select * from board where id = ?";
+
+        try {
+            connection = DBConnectionManager.getConnection();
+            statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, boardId);
+
+            rs = statement.executeQuery();
+            Board board = new Board();
+            while (rs.next()){
+                board.setId(rs.getInt("id"));
+                board.setUser_id(rs.getInt("user_id"));
+                board.setCategory_id(rs.getInt("category_id"));
+                board.setTitle(rs.getString("title"));
+                board.setContent(rs.getString("content"));
+                DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                board.setCreate_at(LocalDateTime.parse(rs.getString("created_at"), format));
+            }
+            return board;
+        } catch (SQLException e) {
+            log.error("selectBoard error={}", e);
+            throw e;
+        } finally {
+            closeResource(connection, statement, rs);
+        }
+    }
+
+    public void updateBoardTitle(Integer id, String title) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        String sql = "update board set title = ? where id = ?";
+
+        try {
+            connection = DBConnectionManager.getConnection();//DriverManger 통해서 DB커넥션 생성
+            statement = connection.prepareStatement(sql);//SQL실행 하기위한 객체 PrepareStatement 생성
+
+            statement.setString(1, title);//DB컬럼과 자바 오브젝트 필드 바인딩
+            statement.setInt(2, id);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            log.error("updateBoardtitle error={}", e);
+            throw e;
+        } finally {
+            closeResource(connection, statement, null);//사용한 리소스 반환
+        }
+    }
+
+    public void deleteBoard(int id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        String sql = "delete from board where id = ?";
+
+        try {
+            connection = DBConnectionManager.getConnection();//DriverManger 통해서 DB커넥션 생성
+            statement = connection.prepareStatement(sql);//SQL실행 하기위한 객체 PrepareStatement 생성
+
+            statement.setInt(1, id);//DB컬럼과 자바 오브젝트 필드 바인딩
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            log.error("deleteBoard error={}", e);
+            throw e;
+        } finally {
+            closeResource(connection, statement, null);//사용한 리소스 반환
+        }
+    }
+
+    private void closeResource(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+        if (resultSet != null){
+            try {
+                resultSet.close();
+            }catch (Exception e){
+                log.error("error", e);
+            }
+        }
+        if (statement != null){
+            try {
+                statement.close();
+            }catch (Exception e){
+                log.error("error", e);
+            }
+        }
+        if (connection != null){
+            try {
+                connection.close();
+            }catch (Exception e){
+                log.error("error", e);
+            }
+        }
+    }
+}
