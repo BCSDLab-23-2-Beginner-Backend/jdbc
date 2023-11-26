570e7742348f179a18ec58d0c12d332130fb75d4
krseonghyeon <ya41921174@gmail.com>
2023-11-26 13:42:10 +0900

    hwk7 final

--- src/main/java/bcsdbeginner/jdbc/DBConnection/DBConnectionConstant.java
+++ src/main/java/bcsdbeginner/jdbc/DBConnection/DBConnectionConstant.java
@@ -7,6 +7,6 @@ public class DBConnectionConstant {
     3306 → MySQL 서버의 포트번호
     community → 사용하는 데이터베이스 명
      */
-    public static final String USERNAME = "root"; // username
-    public static final String PASSWORD = "kk25781179@@"; // password
+    public static final String USERNAME = ""; // username
+    public static final String PASSWORD = ""; // password
 }
