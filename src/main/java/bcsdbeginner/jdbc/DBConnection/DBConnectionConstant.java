522df67b34bd723058fdb00c85752c46ba5e4ef5
Hojin <qkrghwls7@naver.com>
2023-11-27 17:29:04 +0900

    Project

--- src/main/java/bcsdbeginner/jdbc/DBConnection/DBConnectionConstant.java
+++ src/main/java/bcsdbeginner/jdbc/DBConnection/DBConnectionConstant.java
@@ -1,7 +1,7 @@
 package bcsdbeginner.jdbc.DBConnection;
 
 public class DBConnectionConstant {
-    public static final String URL = "jdbc:mysql://localhost:3306/{your db}";
-    public static final String USERNAME = "";
-    public static final String PASSWORD = "";
+    public static final String URL = "jdbc:mysql://localhost:3306/community";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "park2183!";
 }
--- src/test/java/bcsdbeginner/jdbc/repository/BoardRepositoryTest.java
+++ src/test/java/bcsdbeginner/jdbc/repository/BoardRepositoryTest.java
@@ -1,76 +1,63 @@
 package bcsdbeginner.jdbc.repository;
 
 import bcsdbeginner.jdbc.DBConnection.DBConnectionManager;
+import bcsdbeginner.jdbc.domain.Board;
 import bcsdbeginner.jdbc.domain.User;
+import bcsdbeginner.jdbc.repository.BoardRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
-class UserRepositoryTest {
-
-    UserRepository userRepository = new UserRepository();
-
+class BoardRepositoryTest {
+    BoardRepository boardRepository = new BoardRepository();
     //@BeforeEach
     void clearDB() throws SQLException {
         Helper.clearDB(); //테스트하기 전에 다 지우기
     }
-
     @Test
-    void createUser() throws SQLException {
-        User user1 = new User("userA", "bcsd@koreatech.ac.kr", "1111");
-        User newUser = userRepository.createUser(user1);
-        newUser.setCreate_at(LocalDateTime.of(2000, 1, 3, 10, 10, 10));
-        assertThat(newUser.getUsername()).isEqualTo("userA"); // 받아오는 값이랑 기대되는 값이랑 확인 !
+    void createBoard() throws SQLException {
+        Board board1 = new Board(1, 1, "Tired", "Exam");
+        Board newBoard = boardRepository.createBoard(board1);
+        newBoard.setCreate_at(LocalDateTime.of(2000, 1, 3, 10, 10, 10));
+        assertThat(newBoard.getContent()).isEqualTo("Exam"); // 받아오는 값이랑 기대되는 값이랑 확인 !
     }
 
     @Test
-    void findById() throws SQLException {
-        User user1 = new User("userA", "bcsdlab@koreatech.ac.kr", "1111");
-        User newUser = userRepository.createUser(user1);
-
-        User findUser = userRepository.findById(1);
-
-        assertThat(findUser.getId()).isEqualTo(1);
+    void findBoardId() throws SQLException {
+        Board findBoard = boardRepository.findBoardId(1);
+        assertThat(findBoard.getId()).isEqualTo(1);
     }
 
     @Test
-    void updateUsername() throws SQLException {
-        User user1 = new User("userA", "bcsdlab@koreatech.ac.kr", "1111");
-        userRepository.createUser(user1);
-
-        userRepository.updateUsername(1, "updateA");
-        User updateUser = userRepository.findById(1);
-        log.info("user={}", updateUser);
-        assertThat(updateUser.getUsername()).isEqualTo("updateA");
+    void updateBoardname() throws SQLException {
+        boardRepository.updateBoardname(1, "Funny", "Assignment");
+        Board updateBoardname = boardRepository.findBoardId(1);
+        log.info("board={}", updateBoardname);
+        assertThat(updateBoardname.getId()).isEqualTo(1);
     }
 
     @Test
-    void deleteUser() throws SQLException {
-        userRepository.deleteUser(1);
-
-        Integer deleteId = userRepository.findById(1).getId();
+    void deleteBoard() throws SQLException {
+        boardRepository.deleteBoard(1);
+        Integer deleteId = boardRepository.findBoardId(1).getId();
 
         assertThat(deleteId).isNull();
     }
-
     static class Helper {
         public static void clearDB() throws SQLException {
             Connection connection = null;
             Statement statement = null;
-            String sql1 = "delete from users";
-            String sql2 = "alter table users AUTO_INCREMENT = 1";//DB에 넘길 SQL 작성
+            String sql1 = "delete from board";
+            String sql2 = "alter table board AUTO_INCREMENT = 1";//DB에 넘길 SQL 작성
 
             try {
                 connection = DBConnectionManager.getConnection();//DriverManger 통해서 DB커넥션 생성
